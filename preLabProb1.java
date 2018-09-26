4b42fe78a27b689c68c32d168e362acc86acf7f4
davey561 <davey561@gmail.com>
2018-09-25 22:56:08 -0400

    prelabprob1

---
+++ preLabProb1.java
@@ -0,0 +1,9 @@
+class preLabProb1{
+  public static void main(String[] args) {
+    System.out.println(digitSum(1231423412));
+  }
+  public static int digitSum (int i){
+    if(i<10) return i;
+    return (i%10 + digitSum(i/10));
+  }
+}
